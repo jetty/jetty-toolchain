27f4c7fc34ceeadacdc0ad0078aba7524b08d74b
Joakim Erdfelt <joakim@intalio.com>
2010-05-11 22:19:21 +0000

    Renamed existing version rule, and adding 3 new version rules.
    * Debian Version check
    * Redhat Version check
    * OSGi Version check

---
+++ src/main/java/org/eclipse/jetty/toolchain/enforcer/rules/RequireDebianCompatibleVersionRule.java
@@ -0,0 +1,90 @@
+package org.eclipse.jetty.toolchain.enforcer.rules;
+
+//========================================================================
+//Copyright (c) Webtide LLC
+//------------------------------------------------------------------------
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the Eclipse Public License v1.0
+//and Apache License v2.0 which accompanies this distribution.
+//
+//The Eclipse Public License is available at
+//http://www.eclipse.org/legal/epl-v10.html
+//
+//The Apache License v2.0 is available at
+//http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+//You may elect to redistribute this code under either of these licenses.
+//========================================================================
+
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+
+public class RequireDebianCompatibleVersionRule implements EnforcerRule
+{
+
+    public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException
+    {
+        try
+        {
+            String version = (String)helper.evaluate("${project.version}");
+            ensureValidDebianVersion(version);
+        }
+        catch (ExpressionEvaluationException e)
+        {
+            throw new EnforcerRuleException("Unable to lookup an expression " + e.getLocalizedMessage(),e);
+        }
+    }
+
+    public void ensureValidDebianVersion(String version) throws EnforcerRuleException
+    {
+        if (version.endsWith("SNAPSHOT"))
+        {
+            // Skip check on SNAPSHOT versions.
+            return;
+        }
+
+        if (version.contains("-"))
+        {
+            throw new EnforcerRuleException("The version \"" + version + "\" does not conform to the Debian (linux packaging) version requirements.  "
+                    + "It can't have the '-' character as it has a special meaning reserved for the DEB process (package build #).");
+        }
+
+        if (version.contains("_"))
+        {
+            throw new EnforcerRuleException("The version \"" + version + "\" does not conform to the Debian (linux packaging) version requirements.  "
+                    + "It can't have the '_' character as it has a special meaning reserved for the DEB process "
+                    + "(indicates split between package name, version, and architecture on filename).");
+        }
+
+        // Architecture IDs 
+        String reserved[] =
+        { "all", "i386", "i486", "i586", "i686", "pentium", "athlon", "ia64", "x86_64", "amd64", "ia32", "alpha", "sparc", "m68k", "ppc", "hppa", "arm" };
+
+        for (String id : reserved)
+        {
+            if (version.contains(id))
+            {
+                throw new EnforcerRuleException("The version \"" + version + "\" does not conform to the Debian (linux packaging) version requirements.  "
+                        + "It can't use the DEB reserved word \"" + id + "\".");
+            }
+        }
+    }
+
+    public String getCacheId()
+    {
+        return "debian-version";
+    }
+
+    public boolean isCacheable()
+    {
+        return false;
+    }
+
+    public boolean isResultValid(EnforcerRule rule)
+    {
+        return true;
+    }
+
+}
---
+++ src/main/java/org/eclipse/jetty/toolchain/enforcer/rules/RequireOsgiCompatibleVersionRule.java
@@ -0,0 +1,84 @@
+package org.eclipse.jetty.toolchain.enforcer.rules;
+
+//========================================================================
+//Copyright (c) Webtide LLC
+//------------------------------------------------------------------------
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the Eclipse Public License v1.0
+//and Apache License v2.0 which accompanies this distribution.
+//
+//The Eclipse Public License is available at
+//http://www.eclipse.org/legal/epl-v10.html
+//
+//The Apache License v2.0 is available at
+//http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+//You may elect to redistribute this code under either of these licenses.
+//========================================================================
+
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+import org.codehaus.plexus.util.StringUtils;
+
+public class RequireOsgiCompatibleVersionRule implements EnforcerRule
+{
+    public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException
+    {
+        try
+        {
+            String version = (String)helper.evaluate("${project.version}");
+            ensureValidOsgiVersion(version);
+        }
+        catch (ExpressionEvaluationException e)
+        {
+            throw new EnforcerRuleException("Unable to lookup an expression " + e.getLocalizedMessage(),e);
+        }
+    }
+
+    public void ensureValidOsgiVersion(String version) throws EnforcerRuleException
+    {
+        String parts[] = version.split("\\.");
+        if (parts.length > 4)
+        {
+            throw new EnforcerRuleException("The version \"" + version + "\" does not conform to the OSGi version requirements.  "
+                    + "It can't have more than 4 parts (#.#.#.*)" + " - The '.' character has a special meaning");
+        }
+
+        if (parts.length < 3)
+        {
+            throw new EnforcerRuleException("The version \"" + version + "\" does not conform to the OSGi version requirements.  "
+                    + "It must have 3 (or 4) parts (#.#.#.*)");
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (!isNumber(parts[i]))
+            {
+                throw new EnforcerRuleException("The version \"" + version + "\" does not conform to the OSGi version requirements.  " + "Part #" + (i + 1)
+                        + " \"" + parts[i] + "\" of an OSGi version must be a number (#.#.#.*).");
+            }
+        }
+    }
+
+    private boolean isNumber(String str)
+    {
+        return StringUtils.isNumeric(str);
+    }
+
+    public String getCacheId()
+    {
+        return "osgi-version";
+    }
+
+    public boolean isCacheable()
+    {
+        return false;
+    }
+
+    public boolean isResultValid(EnforcerRule rule)
+    {
+        return true;
+    }
+}
