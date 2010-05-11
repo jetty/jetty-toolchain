a52c3541081cd2c9756ae9631fafa9f8f86faa12
Joakim Erdfelt <joakim@intalio.com>
2010-05-11 23:24:46 +0000

    Ignoring pom packaging for version checks

--- src/main/java/org/eclipse/jetty/toolchain/enforcer/rules/RequireDebianCompatibleVersionRule.java
+++ src/main/java/org/eclipse/jetty/toolchain/enforcer/rules/RequireDebianCompatibleVersionRule.java
@@ -28,6 +28,12 @@ public class RequireDebianCompatibleVersionRule implements EnforcerRule
     {
         try
         {
+            String packaging = (String)helper.evaluate("${project.packaging}");
+            if ("pom".equals(packaging))
+            {
+                // Skip pom packaging (not a deployed artifact)
+                return;
+            }
             String version = (String)helper.evaluate("${project.version}");
             ensureValidDebianVersion(version);
         }
