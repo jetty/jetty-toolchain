30196f3b3ab37afd58478060596a3c09d3390597
Joakim Erdfelt <joakim@intalio.com>
2011-03-18 18:53:51 +0000

    Adding some more generic IO and FS utility methods to help out with test-jetty-policy-integration ideas from jesse

--- src/main/java/org/eclipse/jetty/toolchain/test/IO.java
+++ src/main/java/org/eclipse/jetty/toolchain/test/IO.java
@@ -15,8 +15,13 @@ package org.eclipse.jetty.toolchain.test;
 
 import java.io.Closeable;
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -40,7 +45,9 @@ public class IO
         {
             len = in.read(buffer,0,BUFFER_SIZE);
             if (len == -1)
+            {
                 break;
+            }
             out.write(buffer,0,len);
         }
     }
@@ -90,6 +97,81 @@ public class IO
         catch (IOException ignore)
         {
             /* ignore */
+        }
+    }
+
+    /**
+     * Copy files or directories
+     * 
+     * @param from
+     * @param to
+     * @throws IOException
+     */
+    public static void copy(File from, File to) throws IOException
+    {
+        if (from.isDirectory())
+        {
+            copyDir(from,to);
+        }
+        else
+        {
+            copyFile(from,to);
+        }
+    }
+
+    public static void copyDir(File from, File to) throws IOException
+    {
+        FS.ensureDirExists(to);
+
+        for (File file : from.listFiles(new SafeFileFilter()))
+        {
+            copy(file,new File(to,file.getName()));
+        }
+    }
+
+    public static class SafeFileFilter implements FileFilter
+    {
+        public boolean accept(File path)
+        {
+            String name = path.getName();
+            if (".".equals(name) || "..".equals(name))
+            {
+                return false; // old school.
+            }
+            return (path.isFile() || path.isDirectory());
+        }
+    }
+
+    public static void copy(InputStream in, OutputStream out) throws IOException
+    {
+        byte buffer[] = new byte[BUFFER_SIZE];
+        int len = BUFFER_SIZE;
+
+        while (true)
+        {
+            len = in.read(buffer,0,BUFFER_SIZE);
+            if (len < 0)
+            {
+                break;
+            }
+            out.write(buffer,0,len);
+        }
+    }
+
+    public static void copyFile(File from, File to) throws IOException
+    {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try
+        {
+            in = new FileInputStream(from);
+            out = new FileOutputStream(to);
+            copy(in,out);
+        }
+        finally
+        {
+            close(out);
+            close(in);
         }
     }
 }
