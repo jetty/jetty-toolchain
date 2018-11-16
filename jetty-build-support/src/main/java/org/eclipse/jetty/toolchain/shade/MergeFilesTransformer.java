10d0e3d586cf02e38f3895105d398de62917f606
olivier lamy <oliver.lamy@gmail.com>
2018-11-16 17:09:53 +1000

    add a new shade resource transformer to merge content of files in the created archive
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

---
+++ jetty-build-support/src/main/java/org/eclipse/jetty/toolchain/shade/MergeFilesTransformer.java
@@ -0,0 +1,103 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.toolchain.shade;
+
+import org.apache.maven.plugins.shade.relocation.Relocator;
+import org.apache.maven.plugins.shade.resource.ResourceTransformer;
+import org.apache.maven.shared.utils.StringUtils;
+import org.codehaus.plexus.util.IOUtil;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+public class MergeFilesTransformer
+    implements ResourceTransformer
+{
+
+    private List<String> files;
+
+    private String resource;
+
+    /**
+     * if the content of the resource must be preserve
+     * <code>true</code> per default
+     */
+    private boolean preserveResourceContent = true;
+
+    private ByteArrayOutputStream data = new ByteArrayOutputStream();
+
+    public MergeFilesTransformer()
+    {
+        //no op
+    }
+
+    public MergeFilesTransformer( List<String> files, String resource, boolean preserveResourceContent )
+    {
+        this.files = files;
+        this.resource = resource;
+        this.preserveResourceContent = preserveResourceContent;
+    }
+
+    @Override
+    public boolean canTransformResource( String resource )
+    {
+        return StringUtils.equals( this.resource, resource );
+    }
+
+    @Override
+    public void processResource( String resource, InputStream is, List<Relocator> relocators )
+        throws IOException
+    {
+        if ( preserveResourceContent )
+        {
+            IOUtil.copy( is, data );
+            data.write( '\n' );
+        }
+    }
+
+    @Override
+    public boolean hasTransformedResource()
+    {
+        // we need to force shade plugin to modify the JarOutputStream
+        return files.size() > 0;
+    }
+
+    @Override
+    public void modifyOutputStream( JarOutputStream jos )
+        throws IOException
+    {
+        for ( String file : files )
+        {
+            Files.copy( Paths.get( file ), data );
+            data.write( '\n' );
+        }
+
+        jos.putNextEntry( new JarEntry( resource ) );
+
+        IOUtil.copy( new ByteArrayInputStream( data.toByteArray() ), jos );
+        data.reset();
+    }
+}
--- jetty-build-support/src/test/java/org/eclipse/jetty/toolchain/enforcer/rules/RequireOsgiCompatibleVersionRuleTest.java
+++ jetty-build-support/src/test/java/org/eclipse/jetty/toolchain/enforcer/rules/RequireOsgiCompatibleVersionRuleTest.java
@@ -17,97 +17,126 @@ package org.eclipse.jetty.toolchain.enforcer.rules;
 //========================================================================
 
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 public class RequireOsgiCompatibleVersionRuleTest
 {
     @Test
-    public void testValidOsgiVersion() throws EnforcerRuleException
+    public void testValidOsgiVersion()
+        throws EnforcerRuleException
     {
         RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
         // The most basic
-        rule.ensureValidOsgiVersion("1.0.0");
+        rule.ensureValidOsgiVersion( "1.0.0" );
         // The second most basic
-        rule.ensureValidOsgiVersion("1.2.3.4");
+        rule.ensureValidOsgiVersion( "1.2.3.4" );
         // A long form classifier
-        rule.ensureValidOsgiVersion("12.0.0.v20100511-114423");
+        rule.ensureValidOsgiVersion( "12.0.0.v20100511-114423" );
         // A SNAPSHOT version
-        rule.ensureValidOsgiVersion("4.0.2.SNAPSHOT");
+        rule.ensureValidOsgiVersion( "4.0.2.SNAPSHOT" );
         // Some creative versions found "in the wild"
-        rule.ensureValidOsgiVersion("3.4.2.r342_v20081028-0800");
-        rule.ensureValidOsgiVersion("3.4.3.R34x_v20081215-1030");
-        rule.ensureValidOsgiVersion("3.0.5.v20090218-1800-e3x");
-        rule.ensureValidOsgiVersion("1.1.1.M20080827-0800b");
-        rule.ensureValidOsgiVersion("0.1.37.v200803061811");
-    }
-
-    @Test(expected = EnforcerRuleException.class)
-    public void testTooLongOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1.2.3.4.5");
-    }
-
-    @Test(expected = EnforcerRuleException.class)
-    public void testTooShortOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1.2");
+        rule.ensureValidOsgiVersion( "3.4.2.r342_v20081028-0800" );
+        rule.ensureValidOsgiVersion( "3.4.3.R34x_v20081215-1030" );
+        rule.ensureValidOsgiVersion( "3.0.5.v20090218-1800-e3x" );
+        rule.ensureValidOsgiVersion( "1.1.1.M20080827-0800b" );
+        rule.ensureValidOsgiVersion( "0.1.37.v200803061811" );
     }
 
     @Test
-    public void testIgnoredSnapshotOsgiVersion() throws EnforcerRuleException
+    public void testTooLongOsgiVersion()
+        throws EnforcerRuleException
     {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("7.0.2-SNAPSHOT");
-    }
-    
-    @Test(expected = EnforcerRuleException.class)
-    public void testLettersInPartOneOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("a.0.2");
-    }
-    
-    @Test(expected = EnforcerRuleException.class)
-    public void testLettersInPartOneAOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1a.0.2");
-    }
-    
-    @Test(expected = EnforcerRuleException.class)
-    public void testLettersInPartTwoOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1.1a.2");
-    }
-    
-    @Test(expected = EnforcerRuleException.class)
-    public void testBetaOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1.beta.2");
-    }
-    
-    @Test(expected = EnforcerRuleException.class)
-    public void testAlphaOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1.0.alpha-2");
-    }
-    
-    @Test(expected = EnforcerRuleException.class)
-    public void testInvalidQualifierOsgiVersion() throws EnforcerRuleException
-    {
-        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("1.0.2.2009:05:12");
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1.2.3.4.5" );
+        } );
     }
 
     @Test
-    public void testGoodSnapshotOsgiVersion() throws EnforcerRuleException
+    public void testTooShortOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1.2" );
+        } );
+    }
+
+    @Test
+    public void testIgnoredSnapshotOsgiVersion()
+        throws EnforcerRuleException
     {
         RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
-        rule.ensureValidOsgiVersion("7.0.2.SNAPSHOT");
+        rule.ensureValidOsgiVersion( "7.0.2-SNAPSHOT" );
+    }
+
+    @Test
+    public void testLettersInPartOneOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "a.0.2" );
+        } );
+    }
+
+    @Test
+    public void testLettersInPartOneAOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1a.0.2" );
+        } );
+    }
+
+    @Test
+    public void testLettersInPartTwoOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1.1a.2" );
+        } );
+    }
+
+    @Test
+    public void testBetaOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1.beta.2" );
+        } );
+    }
+
+    @Test
+    public void testAlphaOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1.0.alpha-2" );
+        } );
+    }
+
+    @Test
+    public void testInvalidQualifierOsgiVersion()
+        throws EnforcerRuleException
+    {
+        Assertions.assertThrows( EnforcerRuleException.class, () -> {
+            RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+            rule.ensureValidOsgiVersion( "1.0.2.2009:05:12" );
+        } );
+    }
+
+    @Test
+    public void testGoodSnapshotOsgiVersion()
+        throws EnforcerRuleException
+    {
+        RequireOsgiCompatibleVersionRule rule = new RequireOsgiCompatibleVersionRule();
+        rule.ensureValidOsgiVersion( "7.0.2.SNAPSHOT" );
     }
 }
