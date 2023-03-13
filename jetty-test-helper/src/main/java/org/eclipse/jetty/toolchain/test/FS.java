2007e443ffad3c6d40fcb41b53d7dd771d2afb76
Olivier Lamy <oliver.lamy@gmail.com>
2023-03-13 13:50:14 +1000

    unused import
    Signed-off-by: Olivier Lamy <oliver.lamy@gmail.com>

--- jetty-test-helper/src/main/java/org/eclipse/jetty/toolchain/test/FS.java
+++ jetty-test-helper/src/main/java/org/eclipse/jetty/toolchain/test/FS.java
@@ -20,7 +20,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
 import java.util.concurrent.TimeUnit;
 
