f4c53714d48b2cccb54afdc97fa0ee9592a4939f
Olivier Lamy <olamy@apache.org>
2023-03-14 10:48:03 +1000

    Avoid issue on same directory creation when testing in parallel and filter some characters out of display name (#237)
    * ignore FileAlreadyExistsException when creating target/tests directory (can happen with parallel testing)
    * more filtering of displayName from junit to create directories
    
    Signed-off-by: Olivier Lamy <oliver.lamy@gmail.com>

--- jetty-test-helper/src/main/java/org/eclipse/jetty/toolchain/test/MavenPaths.java
+++ jetty-test-helper/src/main/java/org/eclipse/jetty/toolchain/test/MavenPaths.java
@@ -17,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -101,6 +102,10 @@ public final class MavenPaths
             try
             {
                 Files.createDirectory(testsDir);
+            }
+            catch (FileAlreadyExistsException e)
+            {
+                // ignore it as can happen with parallel tests
             }
             catch (IOException e)
             {
@@ -397,7 +402,7 @@ public final class MavenPaths
 
     static String safename(String name)
     {
-        final String reserved = "<>/:\"|\\?*\177";
+        final String reserved = " []<>/:\"|\\?*\177";
         StringBuilder result = new StringBuilder();
         for (char c : name.toCharArray())
         {
